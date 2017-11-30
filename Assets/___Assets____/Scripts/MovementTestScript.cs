1d08e81dad6b4cd7b2dcd7e44fdeee1274348937
tuddor1234 <tuddor1234@yahoo.com>
2017-11-30 18:08:59 +0200

    Initial commit

---
+++ Assets/___Assets____/Scripts/MovementTestScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.UI;
+
+public class MovementTestScript : MonoBehaviour {
+
+    public float force = 5;
+    public Text txt;
+
+    Rigidbody2D rb;
+   
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+	}
+	
+	void FixedUpdate ()
+    {
+        Vector2 moveVec = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal"), CrossPlatformInputManager.GetAxis("Vertical"));
+        print(moveVec);
+        rb.AddForce(moveVec * force);
+        txt.text = moveVec.ToString();
+    }
+}
---
+++ Assets/___Assets____/Scripts/PlayerMovement.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed = 6f;
+    public float turnVel = 12f;
+
+    Vector3 movement;
+    Animator anim;
+    Rigidbody playerRigidbody;
+    int floorMask;
+    float camRayLenght = 100f;
+    float horizontalInput, verticalInput;
+   
+
+
+    private void Awake()
+    {
+        floorMask = LayerMask.GetMask("Floor");
+        anim = GetComponent<Animator>();
+        playerRigidbody = GetComponent<Rigidbody>();
+
+    }
+
+    void FixedUpdate()
+    {
+        //SCHIMBA PENTRU JOYSTICK
+        GetInput();
+        Animating(horizontalInput, verticalInput);
+        Move(horizontalInput, verticalInput);
+    }
+
+    private void Update()
+    {
+        Turn(horizontalInput, verticalInput);
+    }
+
+    void GetInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+
+    void Move(float h,float v)
+    {
+        movement.Set(h, 0, v);
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+
+    void Turn(float h, float v)
+    {
+        // ADD ROTATION
+
+
+    }
+
+    void Animating(float h,float v)
+    {
+        bool walking = h != 0f || v != 0f;
+        anim.SetBool("IsWalking",walking);
+    }
+
+
+
+
+
+}
